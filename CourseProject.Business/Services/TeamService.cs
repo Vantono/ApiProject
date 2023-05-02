5519aaaab527b629679ca1463d96542b315b471c
Vantono <117121346+Vantono@users.noreply.github.com>
2023-05-02 21:53:37 +0300

    Half teamService

---
+++ CourseProject.Business/Services/TeamService.cs
@@ -0,0 +1,12 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CourseProject.Business.Services
+{
+    internal class TeamService
+    {
+    }
+}
